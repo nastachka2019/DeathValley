82ef26d4c8312d10028706c764fcf55857792075
Anastasia Shpakova <shpakavaa@gmail.com>
2020-06-29 18:04:59 +0300

    create tests for service

--- src/main/java/com/devIncubator/task/TestServlet.java
+++ src/main/java/com/devIncubator/task/TestServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+@Deprecated
 @WebServlet
 public class TestServlet extends HttpServlet {
 
--- src/main/java/com/devIncubator/task/connection/ConnectionDemo.java
+++ src/main/java/com/devIncubator/task/connection/ConnectionDemo.java
@@ -3,7 +3,7 @@ package com.devIncubator.task.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+@Deprecated
 public class ConnectionDemo {
     public static void main(String[] args) {
 
